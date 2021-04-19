e191f18c042fb5a87a6b9fa0e7f0e0efb78b6dbb
leonovoleksii <oleksiil@wix.com>
2021-04-19 21:24:10 +0300

    Use list gen for list properties

--- src/test/scala/bencoding4s/BenListParserSpec.scala
+++ src/test/scala/bencoding4s/BenListParserSpec.scala
@@ -5,38 +5,10 @@ import org.scalacheck._
 import scala.util.Random
 import org.scalacheck.Arbitrary.arbitrary
 
-class BenListParserSpec extends Properties("BenListParser") {
+class BenListParserSpec extends Properties("BenListParser") with BenValueGens {
   import Prop.forAll
 
-  property("parse valid one-level bencoded list") = forAll(
-    arbitrary[(List[String], List[BigInt])].suchThat(_._1.forall(_.length > 0))
-  ) { ls: (List[String], List[BigInt]) =>
-    val (ss, is) = ls
-    val shuffledList = Random.shuffle(ss ++ is)
-    val s = "l" ++ shuffledList.map {
-      case i: BigInt => s"i${i}e"
-      case s: String => s"${s.length}:$s"
-    }.foldLeft("")(_ ++ _) ++ "e"
-    val expectedList = shuffledList.map {
-      case i: BigInt => BenInteger(i)
-      case s: String => BenString(s)
-    }
-    benListParser.parse(s) == Right(("", BenList(expectedList)))
-  }
-
-  property("parse list with list inside") = forAll(
-    arbitrary[(List[String], List[BigInt])].suchThat(_._1.forall(_.length > 0))
-  ) { ls: (List[String], List[BigInt]) =>
-    val (ss, is) = ls
-    val shuffledList = Random.shuffle(ss ++ is)
-    val s = "ll" ++ shuffledList.map {
-      case i: BigInt => s"i${i}e"
-      case s: String => s"${s.length}:$s"
-    }.foldLeft("")(_ ++ _) ++ "ee"
-    val expectedList = shuffledList.map {
-      case i: BigInt => BenInteger(i)
-      case s: String => BenString(s)
-    }
-    benListParser.parse(s) == Right(("", BenList(List(BenList(expectedList)))))
+  property("parse bencoded list") = forAll(benListGen(5)) { l: BenList =>
+    benListParser.parse(encode(l)) == Right(("", l))
   }
 }
